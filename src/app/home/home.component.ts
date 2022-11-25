f347ac7e43b11ee472a67e97530022e8ad4b8271
ankesh19 <ankesh.kumar@agoraa.in>
2022-11-25 13:21:04 +0530

    update 26-11-22

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit} from '@angular/core';
 
 @Component({
@@ -7,9 +8,51 @@ import { Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  year=0;
+  emp=0;
+  client=0;
+  project=0;
+
   constructor() { }
 
   ngOnInit(): void {
+   this.animateyear();
+   this.animateemp();
+   this.animateclient();
+   this.animateproject();
+  }
+
+  animateyear(){
+    setInterval(()=>{
+      if(this.year<5)
+        {
+        this.year=this.year+1;
+        } 
+    },500);
+  }
+  animateemp(){
+    setInterval(()=>{
+      if(this.emp<300)
+        {
+        this.emp=this.emp+1;
+        } 
+    },0);
+  }
+  animateclient(){
+    setInterval(()=>{
+      if(this.client<60)
+        {
+        this.client=this.client+1;
+        } 
+    },30);
+  }
+  animateproject(){
+    setInterval(()=>{
+      if(this.project<100)
+        {
+        this.project=this.project+1;
+        } 
+    },20);
   }
 
 }
