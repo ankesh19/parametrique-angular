f051f0cbec9b652ba944e2e974ddfd52821e80e3
ankesh19 <ankesh.kumar@agoraa.in>
2023-01-03 14:19:21 +0530

    code optimization

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'parametrique'`, () => {
+  it(`should have as title 'parametrique-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('parametrique');
+    expect(app.title).toEqual('parametrique-angular');
   });
 
   it('should render title', () => {
